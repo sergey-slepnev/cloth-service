a83edec5fbad096a600f4f17a884c9460a0c7c01
sergey-slepnev <kolybelnaya27ss@gmail.com>
2024-01-16 15:22:23 +0800

    initial commit

---
+++ src/main/java/com/sspdev/clothservice/ClothServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sspdev.clothservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sspdev/clothservice/ClothServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sspdev.clothservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
